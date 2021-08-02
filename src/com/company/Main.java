275639e44822bcc5a73ea9b035c33acbcd6afaf8
Geirby <m2.suboch@gmail.com>
2021-08-02 13:59:18 +0300

    Implement  calculator

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double result = 0;
+        Formatter f = new Formatter();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Pls enter first value");
+        double value1 = in.nextInt();
+        System.out.println("Pls enter second value");
+        double value2 = in.nextInt();
+        System.out.println("Pls enter operator sum(+),mult(*),sub(-) or div(/)");
+        String operator = in.next();
+
+        if (operator.equals("sum"))
+            result = value1 + value2;
+        else if (operator.equals("mult"))
+            result = value1 * value2;
+        else if (operator.equals("sub"))
+            result = value1 - value2;
+        else if (operator.equals("div"))
+            result = value1 / value2;
+        else System.out.println("Wrong value, pls try again");
+
+        System.out.println("Your result  " + f.format("%.2f%n", result));
+
+    }
+
+}
