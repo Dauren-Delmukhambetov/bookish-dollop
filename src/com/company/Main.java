f91cf42991fb59e32d9fcfd686f3dc4309e8131f
Geirby <m2.suboch@gmail.com>
2021-08-02 14:26:43 +0300

    Final version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,25 @@ public class Main {
 
     public static void main(String[] args) {
         double result = 0;
-        Formatter f = new Formatter();
-        Scanner in = new Scanner(System.in); //test
-        System.out.println("Pls enter first value"); //test
-        double value1 = in.nextInt(); //test
+        Scanner in = new Scanner(System.in);
+        System.out.println("Pls enter first value");
+        double value1 = in.nextInt();
         System.out.println("Pls enter second value");
         double value2 = in.nextInt();
         System.out.println("Pls enter operator sum(+),mult(*),sub(-) or div(/)");
         String operator = in.next();
 
-        if (operator.equals("sum"))
+        if (operator.equalsIgnoreCase("sum") || operator.equals("+"))
             result = value1 + value2;
-        else if (operator.equals("mult"))
+        else if (operator.equalsIgnoreCase("mult") || operator.equals("*"))
             result = value1 * value2;
-        else if (operator.equals("sub"))
+        else if (operator.equalsIgnoreCase("sub") || operator.equals("-"))
             result = value1 - value2;
-        else if (operator.equals("div"))
+        else if (operator.equalsIgnoreCase("div") || operator.equals("/"))
             result = value1 / value2;
         else System.out.println("Wrong value, pls try again");
 
-        System.out.println("Your result  " + f.format("%.2f%n", result));
+        System.out.println("Your result  " + String.format("%.2f%n", result));
 
     }
 
