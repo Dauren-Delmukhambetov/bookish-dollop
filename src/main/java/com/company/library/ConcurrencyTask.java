7971c44e714a6cdd1bcf5f5741275bdc4884fa8f
Dauren-Delmukhambetov <dauren.del@gmail.com>
2021-10-07 12:49:02 +0300

    Adding a new class with task todos on Threads

---
+++ src/main/java/com/company/library/ConcurrencyTask.java
@@ -0,0 +1,19 @@
+package com.company.library;
+
+public class ConcurrencyTask {
+
+    public static void main(String[] args) {
+        System.out.printf("%s: This task demos Java Concurrency", Thread.currentThread().getName());
+
+        //todo initialize two instance of CollectionTask that refers to two different input file, e.g. "books.txt", "novels.txt"
+
+        //todo run both task consequently and in parallel (you can comment out of the the options)
+
+        //todo get result of analytics ran on those files as a map of author -> publication collection
+
+        //todo merge the results of two task into one
+
+        //todo print out the result
+    }
+
+}
